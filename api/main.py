7601ff30644b1592359c2474721da9d5ba9e31b2
watson0880 <watson0880@gmail.com>
2023-12-29 03:30:36 +0800

    參考chatgpt加入 supports_credentials=True xhr.withCredentials = true;  // 允许发送凭据

--- api/main.py
+++ api/main.py
@@ -6,8 +6,7 @@ from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 @app.route('/')
 @app.route('/MainPage')
