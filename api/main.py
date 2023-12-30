eec9bfea925fe7c7a7e384d58e20e9a69fb4253b
watson0880 <watson0880@gmail.com>
2023-12-30 10:25:28 +0800

    0

--- api/main.py
+++ api/main.py
@@ -8,10 +8,9 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-app.config['SESSION_TYPE'] = 'null'
+app.config['SESSION_TYPE'] = 'cookie'
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
-app.config.from_object(__name__)
 Session(app)
 CORS(app, supports_credentials=True)
 
